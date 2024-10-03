be01b213b78c4e1f018827e1284cda7db83c9c4d
EyAdOoOoOo <135330383+EyAdOoOoOo@users.noreply.github.com>
2024-10-03 13:03:19 +0500

    Update simple_app.py

--- t5/simple_app.py
+++ t5/simple_app.py
@@ -1,29 +1,31 @@
-import flask
-import subprocess
-import os
-import signal
-import time
+# Importing necessary libraries
+import flask  # Flask web framework
+import subprocess  # For running shell commands
+import os  # For interacting with the operating system
+import signal  # For sending signals to processes
+import time  # For handling time-related functions
 
-from flask import current_app
-
-
+# Initializing a Flask application
 app = flask.Flask(__name__)
 
-@app.endpoint('test')
+# Defining a test endpoint
+@app.route('/test')  # Corrected the decorator to use 'route'
 def test_endpoint():
-    return 'Test endpoint was called!'
+    return 'Test endpoint was called!'  # Response for the endpoint
 
 def start():
-    out = subprocess.getoutput('lsof -i:5000')
-    if out != '':
-        pid = get_pid(out)
-        os.kill(pid, signal.SIGINT)
-        time.sleep(1)
+    # Check if port 5000 is in use
+    out = subprocess.getoutput('lsof -i:5000')  # Get the list of processes using port 5000
+    if out != '':  # If there's an output, it means the port is in use
+        pid = get_pid(out)  # Get the PID of the process using the port
+        os.kill(pid, signal.SIGINT)  # Terminate the process gracefully
+        time.sleep(1)  # Wait for a second to ensure the process has terminated
 
-    app.run()
+    app.run()  # Start the Flask application
 
 def get_pid(s: str) -> int:
-    return int(list(i.split()[1] for i in s.split('\n'))[1:][0])
+    # Extract the PID from the output of lsof command
+    return int(list(i.split()[1] for i in s.split('\n'))[1:][0])  # Return the second line's PID
 
 if __name__ == '__main__':
-    start()
+    start()  # Start the application if the script is run directly
