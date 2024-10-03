57c43d400f08a9de232bfc7f10238d32d3920f57
EyAdOoOoOo <135330383+EyAdOoOoOo@users.noreply.github.com>
2024-10-03 12:36:34 +0500

    Update process_count.py

--- t12/process_count.py
+++ t12/process_count.py
@@ -1,16 +1,31 @@
 import os
 
 def process_count(username: str) -> int:
-    data = os.popen(f'ps --user {username} | wc -l').read()
-    return int(data) - 1
+    """
+    Returns the number of processes currently running for the specified user.
+    """
+    # Run shell command to list processes for the given user and count the lines
+    process_data = os.popen(f'ps --user {username} | wc -l').read()
+    
+    # Subtract 1 to remove the header line from the count and return the result as an integer
+    return int(process_data) - 1
 
 def total_memory_usage(root_pid: int) -> float:
-    process_mem = float(os.popen(f'ps -v {root_pid}').readlines()[1].split()[8])
-    data = os.popen(f'ps -v --ppid {root_pid}').readlines()[1:]
-    raw_arr = list(map(str.split ,data))
-    memory = sum(list(float(i[8]) for i in raw_arr))
-    
-    return process_mem + memory
+    """
+    Returns the total memory usage (in KB) of a process and its child processes.
+    """
+    # Get the memory usage of the root process (column 8 from `ps -v`)
+    process_memory = float(os.popen(f'ps -v {root_pid}').readlines()[1].split()[8])
 
+    # Get memory usage of all child processes (PPID refers to the root process ID)
+    child_processes_data = os.popen(f'ps -v --ppid {root_pid}').readlines()[1:]
+
+    # Extract memory usage for child processes from column 8 and sum the values
+    child_memory = sum(float(proc_info.split()[8]) for proc_info in child_processes_data)
+
+    # Return the total memory usage (root process + child processes)
+    return process_memory + child_memory
+
+# Example usage
 print(f'Root process count: {process_count("root")}')
 print(f'Total memory usage of process_pid 40970 (Firefox): {total_memory_usage(40970)}')
