35e397d4a3d35e411ba0f44f98140db5fed7c098
EyAdOoOoOo <135330383+EyAdOoOoOo@users.noreply.github.com>
2024-10-03 13:02:19 +0500

    Update err_types.py

--- t5/__pycache__/err_types.py
+++ t5/__pycache__/err_types.py
@@ -1,32 +1,37 @@
-from block_errors import BlockErrors
+# Importing the BlockErrors class from the block_errors module
+from block_errors import BlockErrors  
 
+# Define a set of error types to be handled (ZeroDivisionError and TypeError)
 err_types = {ZeroDivisionError, TypeError}
+
+# Using BlockErrors to handle specified errors
 with BlockErrors(err_types):
-    a = 1 / 0
-print('Выполнено без ошибок')
+    a = 1 / 0  # This will raise ZeroDivisionError, but it will be caught by BlockErrors
+print('Completed without errors')  # This line will execute since the error is handled
 
-#Expected out:  Выполнено без ошибок
-
+# Change the error types to only handle ZeroDivisionError
 err_types = {ZeroDivisionError}
+
+# Using BlockErrors to handle specified errors
 with BlockErrors(err_types):
-    a = 1 / '0'
-print('Выполнено без ошибок')
+    a = 1 / '0'  # This will raise TypeError, which is not handled by BlockErrors
+print('Completed without errors')  # This line will NOT execute due to the unhandled TypeError
 
-#Expected out:  TypeError: unsupported operand type(s) for /: 'int' and 'str'
-
+# Define outer error types to handle (TypeError)
 outer_err_types = {TypeError}
+
+# Using BlockErrors to handle outer error types
 with BlockErrors(outer_err_types):
-    inner_err_types = {ZeroDivisionError}
-    with BlockErrors(inner_err_types):
-        a = 1 / '0'
-    print('Внутренний блок: выполнено без ошибок')
-print('Внешний блок: выполнено без ошибок')
+    inner_err_types = {ZeroDivisionError}  # Define inner error types to handle
+    with BlockErrors(inner_err_types):  # Handling inner errors
+        a = 1 / '0'  # This will raise TypeError, which is not handled by inner BlockErrors
+    print('Indoor unit: executed without errors')  # This line will NOT execute
+print('External unit: completed without errors')  # This line will execute since outer error is handled
 
-#Expected out:  Внешний блок: выполнено без ошибок
-
+# Define a set to handle all Exceptions
 err_types = {Exception}
-with BlockErrors(err_types):
-    a = 1 / '0'
-print('Выполнено без ошибок')
 
-#Expected out:  Выполнено без ошибок
+# Using BlockErrors to handle all types of exceptions
+with BlockErrors(err_types):
+    a = 1 / '0'  # This will raise TypeError, but it will be caught by BlockErrors
+print('Completed without errors')  # This line will execute since the error is handled
